c279bea8805de570dac75c9a04dc61f1d9830306
Asif-chohan <rmasifchohan@gmail.com>
2023-02-20 17:32:45 +0500

    slider task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [i1, setI1] = useState(0);
+  const [i2, setI2] = useState(0);
+  const [i3, setI3] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(+i1 + +i2 + +i3);
+  }, [i1, i2, i3]);
+
+  const handleChange = (e, state) => {
+    const input = e.target.value;
+    switch (state) {
+      case 1:
+        const value = 100 - input;
+        setI3(value / 2);
+        setI1(input);
+        setI2(value / 2);
+        break;
+      case 2:
+        const value2 = 100 - input;
+        setI3(value2 / 2);
+        setI1(value2 / 2);
+        setI2(input);
+        break;
+      case 3:
+        const value3 = 100 - input;
+        setI3(input);
+        setI1(value3 / 2);
+        setI2(value3 / 2);
+        break;
+
+      default:
+        break;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Range Slider</h1>
+
+      <input
+        type="range"
+        value={i1}
+        min={0}
+        max={100}
+        onChange={(e) => handleChange(e, 1)}
+      />
+      {i1}
+      <input
+        type="range"
+        value={i2}
+        min={0}
+        max={100}
+        onChange={(e) => handleChange(e, 2)}
+      />
+      {i2}
+      <input
+        type="range"
+        value={i3}
+        min={0}
+        max={100}
+        onChange={(e) => handleChange(e, 3)}
+      />
+      {i3}
+      <h3>Total {total}</h3>
     </div>
   );
 }
